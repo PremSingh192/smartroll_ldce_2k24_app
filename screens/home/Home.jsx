28f67f5604635812b60b05831d945464231c36be
Prem Singh <22cspre036@ldce.ac.in>
2023-12-28 19:47:19 +0530

    printing check on home

--- screens/home/Home.jsx
+++ screens/home/Home.jsx
@@ -1,27 +1,26 @@
 import {View, Text, Alert} from 'react-native';
 // import { AsyncStorage } from 'react-native';
 
-import {  useSelector } from 'react-redux';
+import {useSelector} from 'react-redux';
 import styles from '../../styles/styles';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, {useState, useEffect} from 'react';
 
 const Home = ({navigation}) => {
-  const check = useSelector((state) => state.isLoggedIn);
- useEffect(() => {
+  const check = useSelector(state => state.isLoggedIn);
+  useEffect(() => {
     // Perform navigation after the component has been rendered
-  /* your condition to check */;
-    if (!check) {
+    /* your condition to check */ if (!check) {
       navigation.replace('login');
     }
   }, [navigation]);
- 
- 
 
-  console.log(`check from home ${check}`)
+  console.log(`check from home ${check}`);
   return (
     <View>
-      <Text style={styles.subHeader}>gobal state token{check}</Text>
+      <Text style={styles.subHeader}>
+        gobal state token {check.toString()}
+      </Text>
     </View>
   );
 };
