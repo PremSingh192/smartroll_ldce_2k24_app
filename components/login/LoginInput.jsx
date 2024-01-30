f25b606a4f883652a4d74f5e9194ffd1876755f1
PremSingh192 <premsingh282003@gmail.com>
2024-01-30 17:10:44 +0530

    login page  updated

--- components/login/LoginInput.jsx
+++ components/login/LoginInput.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import {View, Image, TextInput} from 'react-native';
+import React, {useState} from 'react';
+import {View, Image, TextInput, TouchableOpacity} from 'react-native';
 import styles from '../../styles/styles';
 import Svg, {Path, SvgXml} from 'react-native-svg';
 import SvgCall from './GlobalSvg/SvgCall';
-
+import {hide_pass,show_pass} from './GlobalSvg/SvgStore';
 function LoginInput(props) {
+  const eyesvg = props.inputType == 'password' ? true : false;
+  const [changetype, setchangetype] = useState(true);
+  console.log('eyesvg', eyesvg);
   return (
     <View style={styles.bottom_box1_1}>
       <View style={styles.image_icon}>
@@ -13,14 +16,32 @@ function LoginInput(props) {
       {/* <Image source={props.logo_img} style={props.styledClass[0]} /> */}
 
       <TextInput
-      keyboardType={props.keyboardType}
+        keyboardType={props.keyboardType}
         value={props.value}
         onChangeText={props.handlechange}
-        secureTextEntry={props.secret || false}
+        secureTextEntry={props.inputType == 'password' ? changetype : false}
         autoCapitalize="none"
         placeholderTextColor="grey"
         style={[props.styledClass[1], styles.font_global]}
         placeholder={props.placeholderText}></TextInput>
+      {eyesvg && (
+        <TouchableOpacity
+          onPress={() => {
+            console.log('icon clicked');
+            setchangetype(prev => !prev);
+          }}>
+          <View
+            style={{
+              marginVertical: 12,
+              marginRight: 10,
+              width:25,
+              height:25,
+              
+            }}>
+            <SvgCall logo_code={changetype ? show_pass: hide_pass} />
+          </View>
+        </TouchableOpacity>
+      )}
     </View>
   );
 }
--- screens/login/Login.jsx
+++ screens/login/Login.jsx
@@ -1,30 +1,24 @@
 // Import necessary components and libraries
 import React, {useState, useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-
+import Toast from 'react-native-toast-message';
 import logo from '../../assets/images/login-logo.png';
 import base_url from '../../utils/Baseurl';
 import axios from 'axios';
 import {login} from '../../redux/LoginReducer';
-
 import {Image, View} from 'react-native';
-
 import styles from '../../styles/styles';
 import LoginInput from '../../components/login/LoginInput';
 import InputButton from '../../components/login/InputButton';
-
 import {
-  mylogo,
   Email_Svg,
   Pass_Svg,
   Login_logo,
-  Enroll_Svg,
 } from '../../components/login/GlobalSvg/SvgStore';
 import SvgCall from '../../components/login/GlobalSvg/SvgCall';
 import ComponentWrapper from '../../components/ComponentWrapper';
 import {Alert} from 'react-native';
 import Loading from '../../components/Loading';
-
 export default function Login({navigation}) {
   const [loginstate, setloginstate] = useState({
     Email: '',
@@ -33,57 +27,61 @@ export default function Login({navigation}) {
   const [disable, setdisable] = useState(() => false);
   console.log(loginstate);
   const dispatch = useDispatch();
-
   function submitlogin() {
     const {Email, Pass} = loginstate;
-
     if (Email && Pass) {
-      console.log(Email, Pass);
-      setdisable(prev => !prev);
-      console.log(disable);
-      const body = {
-        email: Email,
-        password: Pass,
-      };
-      const axiosconfig = {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      };
-      axios
-        .post(`${base_url}/auth/api/login/`, body, axiosconfig)
-        .then(response => {
-          let data = response.data;
-          if (data.access && data.refresh) {
+      const email = Email.replace(/\s/g, '');
+      const password = Pass.replace(/\s/g, '');
+      if (Email != email || Pass != password) {
+        Alert.alert('Wrong Input', 'Space detected');
+      } else {
+        console.log(Email, Pass);
+        setdisable(prev => !prev);
+        console.log(disable);
+        const body = {
+          email: email,
+          password: password,
+        };
+        const axiosconfig = {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        };
+        axios
+          .post(`${base_url}/auth/api/login/`, body, axiosconfig)
+          .then(response => {
+            let data = response.data;
             if (data.access && data.refresh) {
-              console.log(data.access);
-              // setdisable(prev => data);
-              dispatch(login(data));
-              navigation.replace('home');
+              if (data.access && data.refresh) {
+                console.log(data.access);
+                // setdisable(prev => data);
+                dispatch(login(data));
+                navigation.replace('home');
+              }
             }
-          }
-        })
-        .catch(error => {
-          setdisable(prev => !prev);
-          if (error.response.data.status === 401) {
-            console.log(disable);
-            Alert.alert('error ', `${error.response.data.detail}`);
-          }
-        });
+          })
+          .catch(error => {
+            setdisable(prev => !prev);
+            if (error.response.status === 401) {
+              console.log(disable);
+              Alert.alert('error ', `${error.response.data.detail}`);
+            }
+          });
+      }
     } else {
-      Alert.alert('Input Field Empty', 'please enter email and password');
+      Alert.alert('Empty input', 'Enter Email and Password');
     }
   }
 
   return (
     // wrapper body
+
     <ComponentWrapper>
       {disable && <Loading visible={disable} />}
       <View style={styles.top_container}>
         <Image source={logo} style={styles.image} />
         <SvgCall logo_code={Login_logo}></SvgCall>
       </View>
-
       <View style={styles.bottom_container}>
         <View style={styles.bottom_input_wrapper}>
           <LoginInput
@@ -94,7 +92,7 @@ export default function Login({navigation}) {
                 Email: e,
               });
             }}
-            secret={false}
+            inputType={'text'}
             logo_code={Email_Svg}
             styledClass={[styles.image_, styles.bottom_inner_input]}
             placeholderText={'Enter Email'}
@@ -108,7 +106,7 @@ export default function Login({navigation}) {
                 Pass: e,
               });
             }}
-            secret={true}
+            inputType={'password'}
             logo_code={Pass_Svg}
             styledClass={[styles.image_icon, styles.bottom_inner_input]}
             placeholderText={'Enter Password'}
