5a2bc73c8b2f2bbf927ea0ae957561a4cc2bc34c
PremSingh192 <premsingh282003@gmail.com>
2024-01-30 20:48:26 +0530

    working lottie animation

--- App.jsx
+++ App.jsx
@@ -13,7 +13,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='login'>
+      <Stack.Navigator initialRouteName='home'>
         <Stack.Screen name="home">
           {props => (
             <Router>
--- components/Router.jsx
+++ components/Router.jsx
@@ -1,28 +1,58 @@
 // CustomAuthComponent.js
 
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {useSelector} from 'react-redux';
-import { StyleSheet,View } from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import Loading from './Loading';
 import useAPI from '../hooks/useAPI';
 
-
-const   Router = ({children}) => {  
+const Router = ({children}) => {
   const navigation = useNavigation();
-  const token = useSelector(state => state.isLoggedIn.token);  
-  const [authenticated,setAuthenticated] = useState(false)
-  if(token){
-    useAPI(
-      token, '/auth/api/check_token_authenticity', 'get', {},).then(res => {        
-        setAuthenticated(true)
-      }).catch(err => {        
-        navigation.replace('login')
-      })
-  }    
-  return (
-    authenticated ? <>{children}</> :  <Loading/>
-  );
+  const token = useSelector(state => state.isLoggedIn.token);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      try {
+        if (token) {
+          const res = await useAPI(
+            token,
+            '/auth/api/check_token_authenticity',
+            'get',
+            {},
+          ); // Your API authentication function
+          if (res.response.data.data != true) {
+            console.log('invalid token')
+            setIsLoading(false);
+            navigation.replace('login'); // Redirect to Home screen if not authenticated
+          }
+        } else {
+          setIsLoading(false);
+          console.log('no token');
+          navigation.replace('login');
+        }
+      } catch (error) {
+        setIsLoading(false);
+        console.error('Error checking authentication:', error);
+        navigation.replace('login');
+        // Handle error
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    checkAuthentication();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View>
+        <Loading visible={isLoading} />
+      </View>
+    );
+  }
+
+  return <>{children}</>;
 };
 
 export default Router;
